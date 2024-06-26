433718a2ce253f5ec18cefec8a3c62073d4ce570
who asks? <user@localhost>
2024-06-26 10:31:46 -0600

    reversed modifications, because it doesn't fix the issue complete, further invesetigation neede

--- package/python-idna/python-idna.mk
+++ package/python-idna/python-idna.mk
@@ -9,8 +9,6 @@ PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
 PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438
 PYTHON_IDNA_LICENSE = BSD-3-Clause
 PYTHON_IDNA_LICENSE_FILES = LICENSE.md
-PYTHON_IDNA_DEPENDENCIES = python-tomli python-toml
-HOST_PYTHON_IDNA_DEPENDENCIES = host-python-tomli host-python-toml
 
 PYTHON_IDNA_SETUP_TYPE = flit
 
